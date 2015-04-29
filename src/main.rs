1489e884b649b5a4a25a487ad5287fb8d3dd6f06
Electronic Kiwi <electron.kiwi@gmail.com>
2015-04-29 22:24:12 +0200

    use heap managed strings
    This makes everything so much easier and
    we do not really need performance anyways.
    I normaly would have written this in
    python.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(path_ext)]
+#![feature(collections)]
 
 mod usb;
 
 fn main() {
 	println!("Welcome to emt version 0.1");
 
-	let dev = usb::Device::from_string("3-1.2.1");
+	let dev = usb::Device::from_str("3-1.2.1");
 	println!("{}", dev);
 }
--- src/usb.rs
+++ src/usb.rs
@@ -2,33 +2,33 @@ use std::path::Path;
 use std::fs::PathExt;
 use std::fmt;
 
-pub struct Device<'a> {
-	product: &'a str,
-	manufacturer : &'a str,
-	idProduct: &'a str,
-	idVendor: &'a str
+pub struct Device {
+	product: String,
+	manufacturer : String,
+	idProduct: String,
+	idVendor: String
 }
 
-impl<'a> Device<'a> {
-	pub fn from_path(path: &Path) -> Device<'a> {
+impl Device {
+	pub fn from_path(path: &Path) -> Device {
 		if(!path.exists()) { panic!(format!("Path: {:?} does not exist", path)) };
 		
 		Device {
-			product: "N/A",
-			manufacturer: "N/A",
-			idProduct: "0000",
-			idVendor: "0000"
+			product: String::from_str("N/A"),
+			manufacturer: String::from_str("N/A"),
+			idProduct: String::from_str("0000"),
+			idVendor: String::from_str("0000")
 		}
 	}
 
-	pub fn from_string(s: &str) -> Device<'a> {
+	pub fn from_str(s: &str) -> Device {
 		let path_str = &format!("/sys/bus/usb/devices/{}/", s);
 		let path = Path::new(path_str);
 		Device::from_path(&path)
 	}
 }
 
-impl<'a> fmt::Display for Device<'a> {
+impl fmt::Display for Device {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "{} ({})", self.product, self.manufacturer)
 	}
